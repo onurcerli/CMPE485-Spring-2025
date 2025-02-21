db2bb9d6e22922b502942bb33416596b3c88d895
Onur Çerli <onur.cerli@gmail.com>
2025-02-21 14:03:50 +0300

    Updated the SampleScript to add a force to the ball object each frame for task 2.6

--- homework1/Assets/Scripts/SampleScript.cs
+++ homework1/Assets/Scripts/SampleScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SampleScript : MonoBehaviour
 {
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class SampleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(2, -2, 1);
     }
 }
